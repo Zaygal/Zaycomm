eb072a982cf9a399477c99bccb4be64c269d7b59
Zaygal <140854853+Zaygal@users.noreply.github.com>
2026-08-17 12:21:33 +0100

    Modernize MainApplication for React Native 0.86 host initialization

--- mobile/android/app/src/main/java/com/zaycomm/mobile/MainApplication.kt
+++ mobile/android/app/src/main/java/com/zaycomm/mobile/MainApplication.kt
@@ -1,27 +1,24 @@
 package com.zaycomm.mobile
 
 import android.app.Application
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
-import com.horcrux.svg.SvgPackage
 
 class MainApplication : Application(), ReactApplication {
-    override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> = listOf(
-            SvgPackage(),
-            ZaycommBlePackage()
+    override val reactHost: ReactHost by lazy {
+        getDefaultReactHost(
+            applicationContext,
+            PackageList(this).packages.apply {
+                add(ZaycommBlePackage())
+            }
         )
-
-        override fun getJSMainModuleName(): String = "index"
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
     }
 
-    override val reactHost: ReactHost
-        get() = getDefaultReactHost(applicationContext, reactNativeHost)
+    override fun onCreate() {
+        super.onCreate()
+        loadReactNative(this)
+    }
 }
