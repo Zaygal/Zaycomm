c1cddd5df9e40d6c20a6a100ebe88d587958afdb
Zaygal <140854853+Zaygal@users.noreply.github.com>
2026-08-18 06:19:30 +0100

    Use consolidated five-tab mobile UX

--- mobile/index.js
+++ mobile/index.js
@@ -1,7 +1,7 @@
 import 'react-native-get-random-values';
 import Sentry from './sentry';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './AppV2';
 
 // Must match MainActivity#getMainComponentName().
 AppRegistry.registerComponent('ZaycommMobile', () => Sentry.wrap(App));
