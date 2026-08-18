5a96a708c2724a43f5ce005d130d92b9fc0faffa
Zaygal <140854853+Zaygal@users.noreply.github.com>
2026-08-18 09:13:37 +0100

    Enable immersive edge-to-edge Zaycomm Android shell

--- mobile/android/app/src/main/java/com/zaycomm/mobile/MainActivity.kt
+++ mobile/android/app/src/main/java/com/zaycomm/mobile/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.zaycomm.mobile
 
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,6 +10,17 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
     override fun getMainComponentName(): String = "ZaycommMobile"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        )
+    }
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
