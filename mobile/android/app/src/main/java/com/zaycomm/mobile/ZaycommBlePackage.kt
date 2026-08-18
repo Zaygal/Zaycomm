aa47886b8dce822a657aa4c6bca3d7450d3583f9
Zaygal <140854853+Zaygal@users.noreply.github.com>
2026-08-18 09:25:31 +0100

    Register Zaycomm local notification module

--- mobile/android/app/src/main/java/com/zaycomm/mobile/ZaycommBlePackage.kt
+++ mobile/android/app/src/main/java/com/zaycomm/mobile/ZaycommBlePackage.kt
@@ -7,7 +7,7 @@ import com.facebook.react.uimanager.ViewManager
 
 class ZaycommBlePackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
-        listOf(ZaycommBleModule(reactContext))
+        listOf(ZaycommBleModule(reactContext), ZaycommNotificationModule(reactContext))
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> = emptyList()
 }
