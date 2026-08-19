c240e406832d32a82c9bb791bd0562081dab81e7
Zaygal <140854853+Zaygal@users.noreply.github.com>
2026-08-19 13:20:13 +0100

    feat(test): request camera permission at activity startup

--- mobile/android/app/src/main/java/com/zaycomm/mobile/MainActivity.kt
+++ mobile/android/app/src/main/java/com/zaycomm/mobile/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.zaycomm.mobile
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST = 4101
+    }
+
     override fun getMainComponentName(): String = "ZaycommMobile"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.statusBarColor = Color.TRANSPARENT
         window.navigationBarColor = Color.TRANSPARENT
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M &&
+            checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST
+            )
+        }
     }
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
