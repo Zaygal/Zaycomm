1561f8b1a5fddcda3c9a8f8928c2a10471cc56ff
Zaygal <140854853+Zaygal@users.noreply.github.com>
2026-08-19 08:27:14 +0100

    fix(test): keep top page indicators inside safe area

--- mobile/android/app/src/main/java/com/zaycomm/mobile/MainActivity.kt
+++ mobile/android/app/src/main/java/com/zaycomm/mobile/MainActivity.kt
@@ -2,7 +2,6 @@ package com.zaycomm.mobile
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -15,11 +14,6 @@ class MainActivity : ReactActivity() {
         super.onCreate(savedInstanceState)
         window.statusBarColor = Color.TRANSPARENT
         window.navigationBarColor = Color.TRANSPARENT
-        window.decorView.systemUiVisibility = (
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        )
     }
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
