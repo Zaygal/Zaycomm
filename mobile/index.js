a5b40b7708bb9ea8325109289a3bd6058f028d7e
Zaygal <140854853+Zaygal@users.noreply.github.com>
2026-08-21 20:28:40 +0100

    refactor(app): route native entry through canonical App root

--- mobile/index.js
+++ mobile/index.js
@@ -1,7 +1,7 @@
 import 'react-native-get-random-values';
 import Sentry from './sentry';
 import {AppRegistry} from 'react-native';
-import App from './AppV3';
+import App from './App';
 
 // Must match MainActivity#getMainComponentName().
 AppRegistry.registerComponent('ZaycommMobile', () => Sentry.wrap(App));
