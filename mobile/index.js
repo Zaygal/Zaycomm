0e8b54df59b608e158ed6820366743b7fe569445
Zaygal <140854853+Zaygal@users.noreply.github.com>
2026-08-21 12:11:15 +0100

    feat(pairing): mount home authentication flow

--- mobile/index.js
+++ mobile/index.js
@@ -1,7 +1,7 @@
 import 'react-native-get-random-values';
 import Sentry from './sentry';
 import {AppRegistry} from 'react-native';
-import App from './AppV2';
+import App from './AppV3';
 
 // Must match MainActivity#getMainComponentName().
 AppRegistry.registerComponent('ZaycommMobile', () => Sentry.wrap(App));
